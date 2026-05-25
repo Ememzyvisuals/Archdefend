f87b9251d6d59aeb450398d6465cf5e270d79b91
Ememzyvisuals <ememzylike@gmail.com>
2026-05-25 20:42:58 +0100

    Update env.py

--- apps/api/alembic/env.py
+++ apps/api/alembic/env.py
@@ -21,7 +21,9 @@ from models.models import (  # noqa: F401
 )
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+
+# Escape % characters so configparser doesn't treat them as interpolation syntax
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL.replace("%", "%%"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
